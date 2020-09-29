0e484c1c3f8f972fabb90fc5cee226e7dc60396f
LIN <yi19930716@gmail.com>
2020-09-29 11:49:11 +0800

    test: jwt postman

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const db = require('./models')
+const User = db.User
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
+function authenticated(req, res, next) {
+  passport.authenticate('jwt', { session: false })
+  next()
 };
+app.post('/api/signin', (req, res) => {
+  // 檢查必要資料
+  if (!req.body.email || !req.body.password) {
+    return res.json({ status: 'error', message: "required fields didn't exist" })
+  }
+  // 檢查 user 是否存在與密碼是否正確
+  let username = req.body.email
+  let password = req.body.password
+
+  User.findOne({ where: { email: username } }).then(user => {
+    if (!user) return res.status(401).json({ status: 'error', message: 'no such user found' })
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ status: 'error', message: 'passwords did not match' })
+    }
+    // 簽發 token
+    var payload = { id: user.id }
+    var token = jwt.sign(payload, process.env.JWT_SECRET)
+    return res.json({
+      status: 'success',
+      message: 'ok',
+      token: token,
+      user: {
+        id: user.id, name: user.name, email: user.email, role: user.role
+      }
+    })
+  })
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
